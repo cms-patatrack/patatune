7799ca1ab8184342adda474c1033e556447840ce
Simone Rossi Tisbeni <simone.rossitisbeni@unibo.it>
2025-09-17 15:35:18 +0200

    Add asynchronous evaluation support in BatchObjective and AsyncElementWiseObjective; implement tests for ZDT1 and Binh-Korn objectives

--- src/patatune/__init__.py
+++ src/patatune/__init__.py
@@ -3,7 +3,7 @@ A Python package that implements different type of Optimization Algorithms
 """
 from .util import FileManager, Randomizer, Logger
 from .optimizer import Optimizer
-from .objective import Objective, ElementWiseObjective, BatchObjective
+from .objective import Objective, ElementWiseObjective, BatchObjective, AsyncElementWiseObjective
 from .mopso.mopso import MOPSO
 from . import metrics
 
